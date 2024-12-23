5bc83bdf4d4636bbd0e7a1adf1ca8f0724eb0321
ShunKuroiwa0000 <shunkuro.sk@gmail.com>
2024-12-23 21:29:04 +0900

    make_pdf_jp.py日本語のみバージョン完了

--- merge_pdf.py
+++ merge_pdf.py
@@ -1,21 +1,26 @@
 import PyPDF2 as pp2
 import glob
+import sys
 
-# PDFファイルが入っているフォルダのパス
-path = r"C:\Users\shunk\Downloads\proceedings_short"
+def merge_pdfs(path):
+    # フォルダ内の全PDFファイルを読み込み
+    pdf_list = glob.glob(fr"{path}\*.pdf")
 
-# フォルダ内の全PDFファイルを読み込み
-pdf_list = glob.glob(fr"{path}\*.pdf")
+    # 結合機能を呼び出し
+    merger = pp2.PdfMerger()
 
-# 結合機能を呼び出し
-merger = pp2.PdfMerger()
+    # ループで全PDFファイルを結合機能へ格納
+    for pdf in pdf_list:
+        merger.append(pdf)
 
-# ループで全PDFファイルを結合機能へ格納
-for pdf in pdf_list:
-  merger.append(pdf)
+    # 結合して保存する
+    merger.write(fr"{path}\merged.pdf")
 
-# 結合して保存する
-merger.write(fr"{path}\merged.pdf")
+    # 結合機能を閉じる←必要
+    merger.close()
 
-# 結合機能を閉じる←必要
-merger.close()
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python merge_pdf.py <path_to_pdf_folder>")
+    else:
+        merge_pdfs(sys.argv[1])
